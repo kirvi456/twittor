0fd56641b7d5e0b0e308051378cc1dbe5075ac4c
kirvi456 <kirvi16@gmail.com>
2022-07-05 15:47:52 -0600

    modificado la ruta de sw

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js')
 
-const STATIC_CACHE      = 'static-v2';
-const DYNAMIC_CACHE     = 'dynamic-v2';
+const STATIC_CACHE      = 'static-v3';
+const DYNAMIC_CACHE     = 'dynamic-v3';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -48,6 +48,10 @@ self.addEventListener('activate', e => {
     .then(keys => {
         keys.forEach(key => {
             if( key !== STATIC_CACHE && key.includes('static') ){
+                caches.delete( key )
+            }
+
+            if( key !== DYNAMIC_CACHE && key.includes('dynamic') ){
                 caches.delete( key )
             }
         });
