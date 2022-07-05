266ecc47c03b8489fa8bd3a65271777a72bfa2ed
kirvi456 <kirvi16@gmail.com>
2022-07-05 12:45:01 -0600

    modificado la ruta de sw

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+let url = window.location.href;
+let swLocation = '/twittor/sw.js'
+
 if( navigator.serviceWorker ) {
-    navigator.serviceWorker.register( '/sw.js' );
+    if(url.includes('localhost')) swLocation = '/sw.js'
+    navigator.serviceWorker.register( swLocation );
 }
 
 
